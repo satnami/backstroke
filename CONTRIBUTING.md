6a6def60ed3ad270d1e972e1c1a0763d1aef1554
Mhd Sami Almouhtaseb <mssatnami@gmail.com>
2017-10-09 11:43:28 +0800

    Update from upstream repo 1egoman/backstroke

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,37 +1,19 @@
-# Contributing to Backstroke
+# Subcommands
 
-## What is "open work"?
-At http://gratipay.com/about, they have a definition:
-
-> “Open work” means that your company or organization makes it easy for any individual to do your work for you without asking you first, and as a result to share in any revenue you generate. 
-
-Do work first, ask permission later. Fun, isn't it? If you are opening a new issue or submitting a pull request, **go for it!** Don't be afraid that it's a dumb idea or a duplicate of another issue or an unwanted change or whatever. Maybe it is! We're still glad to have you! :^)
-
-## How you can contribute
-We're always looking for ways to make Backstroke better. Here's a few:
-
-1. Follow along on GitHub. Looks like you're in the right place already!
-2. Diagnose a bug by creating an issue
-3. Suggest an improvement by creating an issue
-4. Close resolved issues ([here's the big list](https://github.com/1egoman/backstroke/issues?q=is%3Aopen+is%3Aissue))
-5. Fix a bug/resolve an issue
-6. Write a test (or a few)
-7. Add a comment to the code
-8. Create an example
-9. Donate on Gratipay. Servers cost money, and anything dontated will be used to keep Backstroke
-   running. <https://gratipay.com/Backstroke/>
-
-## Setting up Backstroke for development
-1. First, install biome: <http://github.com/1egoman/biome>
-2. Clone the repo from Github onto a local machine.
-3. Open up two terminals, both `cd`ed into Backstroke's root.
-4. In both terminals, run `biome`. Biome manages environment variables and this command allows you
-   to enter the value of each variable Backstroke requires.
-5. After entering in the required pieces of information, run `biome use` in both terminals.
-6. Lastly, in one terminal, run `npm start`. In the other terminal, run `gulp`.
-7. By default, Backstroke listens on port `8000`. Unless you changed that in biome, open a web
-   browser to `http://localhost:8000`.
-8. Backstroke's running! :tada:
-
-### More info
-Hi, I'm Ryan. Want to get ahold of me? Check out [my website](http://rgaus.net).
+- `yarn test`: Run all tests interactively with jest.
+- `yarn coverage`: Get coverage statistics with jest.
+- `yarn migrate`: Perform a database migration to get the database tables into the expected state
+  given the state of the models.
+- `yarn shell`: Opens a REPL with a number of helpful utilities, including:
+  - `redis` is an instance of node-redis that is connected to the redis instance used by the server
+    and worker.
+  - `Link` is an instance of the `Link` model that is attached to the database. Useful for performing
+    database operations on links.
+  - `User` is an instance of the `User` model that is attached to the database. Useful for performing
+    database operations on links.
+  - `WebhookQueue` is an object containing `.push(item)` and `.pop()` functions used to add or
+    remove items from the queue.
+  - `WebhookStatusStore` is an object containing `.set(id, data)` and `.get(id)` functions used to
+    get the status of a webhook operation given its id.
+- `yarn manual-job`: Manually issue the webhook job. Primarily used if working on the webhook job
+  and one wants to run the job over and over.
--- src/server.js
+++ src/server.js
@@ -1,44 +1,100 @@
 import express from 'express';
-let app = express();
-app.set('view engine', 'ejs');
-app.use(express.static('build'));
+const app = express();
+
+import cors from 'cors';
+const corsHandler = cors({
+  origin(origin, callback) {
+    if (!origin || origin.match(new RegExp(process.argv.CORS_ORIGIN_REGEXP, 'i'))) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  },
+  credentials: true,
+});
+app.use(corsHandler);
+app.options('*', corsHandler);
+
+// Polyfill promise with bluebird.
 import Promise from 'bluebird';
+global.Promise = Promise;
 
-import whoami from 'controllers/whoami';
-import * as links from 'controllers/links';
-import {checkRepo} from 'controllers/checkRepo';
-import webhook from 'controllers/webhook';
-import webhookOld from 'controllers/webhookOld';
-import {getSubscriptionInformation, updatePaidLinks, addPaymentToUser} from 'controllers/payments';
-
-import isLinkPaid from 'helpers/isLinkPaid';
-import {addWebhooksForLink, removeOldWebhooksForLink} from 'helpers/addWebhooksForLink';
-
-// ----------------------------------------------------------------------------
-// Mongo stuff
-// ----------------------------------------------------------------------------
-import mongoose from 'mongoose';
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_URI);
-mongoose.Promise = Promise;
-import User from 'models/User';
-import Link from 'models/Link';
+// How should we redirect to other origins? If unset, add some mocks to this app to use as those
+// redirects.
+const APP_URL = process.env.APP_URL || '/mocks/app';
+if (APP_URL === '/mocks/app') {
+  app.get('/mocks/app', (req, res) => res.send('This would redirect to the app when deployed.'));
+}
+const ROOT_URL = process.env.ROOT_URL || '/mocks/root';
+if (APP_URL === '/mocks/root') {
+  app.get('/mocks/root', (req, res) => res.send('This would redirect to the main site when deployed.'));
+}
+app.get('/', (req, res) => res.sendFile(__dirname+'/root-file.html'));
 
 // ----------------------------------------------------------------------------
-// Set up session store
+// Routes and helpers for the routes
 // ----------------------------------------------------------------------------
-const MongoStore = require('connect-mongo')(session),
-mongoStore = new MongoStore({mongooseConnection: mongoose.connection});
+import route from './helpers/route';
+
+import whoami from './routes/whoami';
+import checkRepo from './routes/checkRepo';
+
+import manual from './routes/webhook/manual';
+import status from './routes/webhook/status';
+import webhookDispatcherJob from './jobs/webhook-dispatcher';
+import fetchSHAForUpstreamBranch from './jobs/webhook-dispatcher/fetch-sha-for-upstream-branch';
+import { isCollaboratorOfRepository } from './routes/links/update/helpers';
+
+import linksList from './routes/links/list';
+import linksGet from './routes/links/get';
+import linksCreate from './routes/links/create';
+import linksDelete from './routes/links/delete';
+import linksUpdate from './routes/links/update';
+import linksEnable from './routes/links/enable';
+
+import { Link, User, WebhookQueue, WebhookStatusStore } from './models';
+
+/* app.use((err, req, res, next) => { */
+/*   if (err.name === 'ValidationError') { */
+/*     res.status(err.statusCode).send({ */
+/*       ok: false, */
+/*       error: 'validation', */
+/*       context: err.context, */
+/*       issues: err.codes, */
+/*     }); */
+/*   } else { */
+/*     console.error(err.stack); */
+/*     res.status(500).send(err.stack); */
+/*   }; */
+/* }) */
+
+// Use sentry in production
+import Raven from 'raven';
+if (process.env.SENTRY_CONFIG) {
+  Raven.config(process.env.SENTRY_CONFIG).install();
+}
+
+// ----------------------------------------------------------------------------
+// Start the webhook job to add to the queue automatically
+// ----------------------------------------------------------------------------
+if (require.main === module) {
+  console.log('Starting webhook jobs...');
+  webhookDispatcherJob(Link, User, WebhookQueue, fetchSHAForUpstreamBranch);
+}
 
 // ----------------------------------------------------------------------------
 // Passport stuff
 // ----------------------------------------------------------------------------
 import passport from 'passport';
 import session from 'express-session';
-import strategy from 'auth/strategy';
-import serialize from 'auth/serialize';
+import fileStore from 'session-file-store';
+const FileStore = fileStore(session);
+import strategy from './auth/strategy';
+import serialize from './auth/serialize';
 app.use(session({
   secret: process.env.SESSION_SECRET,
-  store: mongoStore,
+  saveUninitialized: true,
+  resave: true,
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -52,75 +108,132 @@ app.use(morgan('tiny'));
 // Authenticate a user
 app.get('/setup/login', passport.authenticate('github', {
   successRedirect: '/',
-  failureRedirect: '/setup/login',
   scope: ["repo", "write:repo_hook", "user:email"],
 }));
 app.get('/setup/login/public', passport.authenticate('github', {
   successRedirect: '/',
-  failureRedirect: '/setup/login',
   scope: ["public_repo", "write:repo_hook", "user:email"],
 }));
 
 // Second leg of the auth
 app.get("/auth/github/callback", passport.authenticate("github", {
-  failureRedirect: '/setup/login',
+  failureRedirect: '/setup/failed',
 }), (req, res) => {
-  res.redirect('/#/links'); // on success
+  res.redirect(APP_URL); // on success
+});
+
+// Second leg of the auth
+app.get("/auth/github-public/callback", passport.authenticate("github-public", {
+  failureRedirect: '/setup/failed',
+}), (req, res) => {
+  res.redirect(APP_URL); // on success
 });
 
 app.get('/logout', (req, res) => {
   req.logout();
-  res.redirect('/');
+  res.redirect(ROOT_URL);
 });
 
-// identify the currently logged in user
-app.get('/api/v1/whoami', whoami);
+// A utility function to check if a user is authenticated, and if so, return
+// the authenticated user. Otherwise, this function will throw an error
+function assertLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.status(401).send({error: 'Not authenticated.'});
+  }
+}
 
-// get all links
-app.get('/api/v1/links', bodyParser.json(), links.index.bind(null, Link));
+import GitHubApi from 'github';
+function constructor(github) {
+  return {
+    reposGet: Promise.promisify(github.repos.get),
+    reposGetBranch: Promise.promisify(github.repos.getBranch),
+    reposGetBranches: Promise.promisify(github.repos.getBranches),
+    reposGetForks: Promise.promisify(github.repos.getForks),
+    reposFork: Promise.promisify(github.repos.fork),
+    reposEdit: Promise.promisify(github.repos.edit),
+    reposDelete: Promise.promisify(github.repos['delete']),
+    reposMerge: Promise.promisify(github.repos.merge),
+    reposAddCollaborator: Promise.promisify(github.repos.addCollaborator),
+
+    pullRequestsCreate: Promise.promisify(github.pullRequests.create),
+    pullRequestsGetAll: Promise.promisify(github.pullRequests.getAll),
+    pullRequestsMerge: Promise.promisify(github.pullRequests.merge),
+
+    reposCreateHook: Promise.promisify(github.repos.createHook),
+    reposDeleteHook: Promise.promisify(github.repos.deleteHook),
+    reposFork: Promise.promisify(github.repos.fork),
+    reposGetCollaborators: Promise.promisify(github.repos.getCollaborators),
+    searchIssues: Promise.promisify(github.search.issues),
+  };
+}
+
+// Authorize the bot.
+const bot = new GitHubApi({});
+bot.authenticate({
+  type: "oauth",
+  token: process.env.GITHUB_TOKEN,
+});
+
+// An express middleware that adds a github api instance to the request for the currently
+// authenticated user. If no user is logged in, the property isn't set.
+function authedGithubInstance(req, res, next) {
+  req.github = {};
+
+  // Add the bot api instance to the request.
+  req.github.bot = constructor(bot);
+
+  // If a user is logged in, create an add a user instance.
+  if (req.user) {
+    const github = new GitHubApi({});
+    github.authenticate({
+      type: "oauth",
+      token: req.user.accessToken,
+    });
+    
+    req.github.user = constructor(github);
+  }
+  return next();
+}
+
+// Redirect calls to `/api/v1` => `/v1`
+app.all(/^\/api\/v1\/.*$/, (req, res) => res.redirect(req.url.replace(/^\/api/, '')));
+
+// Identify the currently logged in user
+app.get('/v1/whoami', whoami);
+
+// GET all links
+app.get('/v1/links', bodyParser.json(), assertLoggedIn, route(linksList, [Link]));
 
 // GET a given link
-app.get('/api/v1/links/:id', bodyParser.json(), links.get.bind(null, Link));
+app.get('/v1/links/:id', bodyParser.json(), assertLoggedIn, route(linksGet, [Link]));
+
+// Create a new link
+app.post('/v1/links', bodyParser.json(), assertLoggedIn, route(linksCreate, [Link]));
 
 // delete a link
-app.delete('/api/v1/links/:id', links.del.bind(null, Link, User, updatePaidLinks));
+app.delete('/v1/links/:id', assertLoggedIn, route(linksDelete, [Link]));
 
 // return the branches for a given repo
-app.get('/api/v1/repos/:provider/:user/:repo', bodyParser.json(), checkRepo);
-
-// create a new link
-app.post('/api/v1/links', bodyParser.json(), links.create.bind(null, Link));
+app.get('/v1/repos/:provider/:user/:repo', bodyParser.json(), assertLoggedIn, authedGithubInstance, checkRepo);
 
 // POST link updates
-app.post('/api/v1/links/:linkId', bodyParser.json(),
-  links.update.bind(null, Link, User, isLinkPaid, addWebhooksForLink, removeOldWebhooksForLink, updatePaidLinks));
-
-// get the info to the currently subscribed plan
-app.get('/api/v1/subscribed', getSubscriptionInformation);
-
-// add a card to an account
-// body: {"source": "tok_stripetoken", "email": "billing@email.com"}
-app.post('/api/v1/payments', bodyParser.json(), addPaymentToUser.bind(null, User));
+app.post('/v1/links/:linkId', bodyParser.json(), assertLoggedIn, route(linksUpdate, [Link, isCollaboratorOfRepository]));
 
 // enable or disable a repository
-app.post('/api/v1/link/:linkId/enable', bodyParser.json(), links.enable.bind(null, Link, User, updatePaidLinks));
+app.post('/v1/links/:linkId/enable', bodyParser.json(), route(linksEnable, [Link]));
 
-// the old webhook route
-// This parses the body of the request to get most of its data.
-app.post("/", bodyParser.json(), webhookOld);
-app.route("/ping/github/:user/:repo").get((req, res) => {
-  res.redirect(`https://github.com/${req.params.user}/${req.params.repo}`);
-}).post(webhook);
+// the new webhook route - both the condensed verson meant to be called by a user and the interal
+// variant that follows REST a bit closer.
+app.all('/_:linkId', route(manual, [Link, User, WebhookQueue]));
+// app.post('/v1/webhooks/:linkId/update', assertLoggedIn, route(manual, [Link, User, WebhookQueue]));
 
-// the new webhook route
-// No body parsing, all oauth-based
-app.all('/_:linkId', webhook.bind(null, Link));
+// check to see how a link operation is doing after it has been kicked off
+app.get('/v1/operations/:operationId', route(status, [WebhookStatusStore]));
 
-// For letsencrypt
-app.get('/.well-known/acme-challenge/:id', (req, res) =>
-  res.status(200).send(process.env.LETSENCRYPT_ID)
-);
-
-let port = process.env.PORT || 8001;
-app.listen(port);
-console.log("Listening on port", port, "...");
+if (require.main === module) {
+  const port = process.env.PORT || 8001;
+  app.listen(port);
+  console.log('Listening on port', port);
+}
